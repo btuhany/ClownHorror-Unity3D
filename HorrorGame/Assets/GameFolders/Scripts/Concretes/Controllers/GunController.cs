0dd26f6a0e4cd9f6cf05837d7e70b3a279597ef9
btuhany <114235184+btuhany@users.noreply.github.com>
2023-03-23 16:53:32 +0300

    Gun Fx controllers

--- HorrorGame/Assets/GameFolders/Scripts/Concretes/Controllers/GunController.cs
+++ HorrorGame/Assets/GameFolders/Scripts/Concretes/Controllers/GunController.cs
@@ -1,113 +1,127 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+using Managers;
 
-public class GunController : MonoBehaviour
+namespace Controllers
 {
-    [Header("FX Transforms")]
-    [SerializeField] Transform _barrelTransform;
-    [Header("Shooting")]
-    [SerializeField] private float _range = 50f;
-    [SerializeField] private Camera _fpsCam;
-    [SerializeField] LayerMask _layerMask;
-
-    [Header("Camera Movement At Aim")]
-    [SerializeField] Transform _defaultGunPos;
-    [SerializeField] Transform _aimingGunPos;
-    [SerializeField] float _camFovAtAim;
-    [SerializeField] float _gunAimPosLerpSpeed;
-    [SerializeField] float _gunDefaultPosLerpSpeed;
-    [SerializeField] float _aimCamFOVLerpSpeed;
-    [SerializeField] float _defaultFOVCamLerpSpeed;
-
-
-    private bool _onTransitionToAimCam;
-    private bool _isOnDefaultCam;
-
-    [SerializeField]  RecoilEffectHandler _recoil;
-
-    Animator _anim;
-    SwayHandler _swayHandler;
-    private bool _cancelSway;
-    float _defaultFov;
-
-   
-
-    private void Awake()
+    public class GunController : MonoBehaviour
     {
-        _anim= GetComponent<Animator>();
-        _swayHandler = GetComponent<SwayHandler>();
-        _defaultFov = _fpsCam.fieldOfView;
-        transform.position = _defaultGunPos.position;
-    }
-    private void Update()
-    {
-        
+        [SerializeField] RecoilEffectHandler _recoil;
+        [Header("FX")]
+        [SerializeField] Transform _barrelTransform;
+        [SerializeField] Transform _casinExitTransform;
+        [SerializeField] float _shotPower;
+        [SerializeField] private float ejectPower = 150f;
+        [Header("Shooting")]
+        [SerializeField] private float _range = 50f;
+        [SerializeField] private Camera _fpsCam;
+        [SerializeField] LayerMask _layerMask;
+
+        [Header("Camera Movement At Aim")]
+        [SerializeField] Transform _defaultGunPos;
+        [SerializeField] Transform _aimingGunPos;
+        [SerializeField] float _camFovAtAim;
+        [SerializeField] float _gunAimPosLerpSpeed;
+        [SerializeField] float _gunDefaultPosLerpSpeed;
+        [SerializeField] float _aimCamFOVLerpSpeed;
+        [SerializeField] float _defaultFOVCamLerpSpeed;
+
+
+        private bool _onTransitionToAimCam;
+        private bool _isOnDefaultCam;
+        float _defaultFov;
+        Animator _anim;
+        SwayHandler _swayHandler;
+
+        private void Awake()
+        {
+            _anim = GetComponent<Animator>();
+            _swayHandler = GetComponent<SwayHandler>();
+            _defaultFov = _fpsCam.fieldOfView;
+            transform.position = _defaultGunPos.position;
+        }
+        private void Update()
+        {
+            if (_onTransitionToAimCam)
+            {
+                _swayHandler.AimSway();
+            }
+            else if (_isOnDefaultCam)  //dont check onTransitionToDefaultCam because _swayHandler.WeaponSway also manipulates postion therefore prevents the transition.
+            {
+                _swayHandler.WeaponSway();
+            }
+        }
+        private void InstantiateMuzzleFX()
+        {
+            GameObject newObjMuzzleFx = ObjectPoolManager.Instance.GetObjectFromPool(_barrelTransform, PoolObjectId.MuzzleFlashFx);
+            StartCoroutine(SetToPool(newObjMuzzleFx, 0.5f, PoolObjectId.MuzzleFlashFx));
+        }
+        private void InstantiateBullet()
+        {
+            GameObject newObjBullet = ObjectPoolManager.Instance.GetObjectFromPool(_barrelTransform, PoolObjectId.Bullet);
+            newObjBullet.GetComponent<Rigidbody>().AddForce(_barrelTransform.forward * _shotPower);
+            StartCoroutine(SetToPool(newObjBullet, 0.7f, PoolObjectId.Bullet));
+        }
+        public void Shoot()
+        {
+            _anim.SetTrigger("Fire");
+            _recoil.RecoilEffect();
+            InstantiateMuzzleFX();
+            InstantiateBullet();
+            if (Physics.Raycast(_fpsCam.transform.position, _fpsCam.transform.forward, out RaycastHit hit, _range, _layerMask))
+            {
+
+
+
+            }
+        }
+        public void AimCam()
+        {
+            _isOnDefaultCam = false;
+            if (Vector3.Distance(transform.position, _aimingGunPos.position) < 0.2f && Mathf.Abs(_camFovAtAim - _fpsCam.fieldOfView) < 0.02f) return;
+            _onTransitionToAimCam = true;
+            transform.position = Vector3.Lerp(transform.position, _aimingGunPos.position, Time.deltaTime * _gunAimPosLerpSpeed);
+            _fpsCam.fieldOfView = Mathf.Lerp(_fpsCam.fieldOfView, _camFovAtAim, Time.deltaTime * _aimCamFOVLerpSpeed);
+
+
+        }
+        public void DefaultCam()
+        {
+            if (Vector3.Distance(transform.position, _defaultGunPos.position) < 0.03f && Mathf.Abs(_defaultFov - _fpsCam.fieldOfView) < 0.02f)
+            {
+                _isOnDefaultCam = true;
+
+                return;
+            }
+
+            _onTransitionToAimCam = false;
+            _swayHandler.WeaponSwayTime = 5.5f;
+
+            transform.position = Vector3.Lerp(transform.position, _defaultGunPos.position, Time.deltaTime * _gunDefaultPosLerpSpeed);
+            _fpsCam.fieldOfView = Mathf.Lerp(_fpsCam.fieldOfView, _defaultFov, Time.deltaTime * _defaultFOVCamLerpSpeed);
+
+        }
+
+        public void BulletCasingFx() //trigger on the animation event
+        {
+            Debug.Log("deneme");
             
-        if(_onTransitionToAimCam)
-        {
-            _swayHandler.AimSway();
-        }
-        else if(_isOnDefaultCam)  //dont check onTransitionToDefaultCam because _swayHandler.WeaponSway also manipulates postion therefore prevents the transition.
-        {
-            _swayHandler.WeaponSway();
-        }
-            
-    
-
+            GameObject tempCasing = ObjectPoolManager.Instance.GetObjectFromPool(_casinExitTransform, PoolObjectId.BulletCasin);
+          
+         
+            tempCasing.GetComponent<Rigidbody>().AddExplosionForce(Random.Range(ejectPower * 0.7f, ejectPower), (_casinExitTransform.position - _casinExitTransform.right * 0.3f - _casinExitTransform.up * 0.6f), 1f);
            
-        
-    }
-    public void Shoot()
-    {
+            tempCasing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
 
-        _recoil.RecoilEffect();
-        if (Physics.Raycast(_fpsCam.transform.position, _fpsCam.transform.forward, out RaycastHit hit, _range, _layerMask))
+
+            StartCoroutine(SetToPool(tempCasing, 1f, PoolObjectId.BulletCasin));
+        }
+        IEnumerator SetToPool(GameObject gameObj, float delay, PoolObjectId objectID)
         {
-            GameObject newObj = ObjectPoolManager.Instance.GetObjectFromPool(PoolObjectId.MuzzleFlashFx);
-            newObj.transform.position = _barrelTransform.position;
-            newObj.transform.rotation = _barrelTransform.rotation;
-            newObj.SetActive(true);
+            yield return new WaitForSeconds(delay);
+            ObjectPoolManager.Instance.SetPool(gameObj, objectID);
+            yield return null;
         }
     }
-    public void AimCam()
-    {
-        _isOnDefaultCam = false;
-        if (Vector3.Distance(transform.position, _aimingGunPos.position) < 0.2f && Mathf.Abs(_camFovAtAim - _fpsCam.fieldOfView) < 0.02f) return;
-        _onTransitionToAimCam = true;
-        transform.position = Vector3.Lerp(transform.position, _aimingGunPos.position, Time.deltaTime * _gunAimPosLerpSpeed);
-        _fpsCam.fieldOfView = Mathf.Lerp(_fpsCam.fieldOfView, _camFovAtAim, Time.deltaTime * _aimCamFOVLerpSpeed);
-       
-        
-    }
-    public void DefaultCam()
-    {
-        if (Vector3.Distance(transform.position, _defaultGunPos.position) < 0.03f && Mathf.Abs(_defaultFov - _fpsCam.fieldOfView) < 0.02f)
-        {
-            _isOnDefaultCam = true;
-           
-            return;
-        }
-        
-        _onTransitionToAimCam = false;
-        _swayHandler.WeaponSwayTime = 5.5f;
-        
-        transform.position = Vector3.Lerp(transform.position, _defaultGunPos.position, Time.deltaTime * _gunDefaultPosLerpSpeed);
-        _fpsCam.fieldOfView = Mathf.Lerp(_fpsCam.fieldOfView, _defaultFov, Time.deltaTime * _defaultFOVCamLerpSpeed);
-        
-    }
 
-
-
-
-    void CasingRelease()
-    {
-
-    }
-    private void GunRecoil()
-    {
-        
-    }
 }
--- HorrorGame/Assets/GameFolders/Scripts/ObjectPoolManager.cs
+++ HorrorGame/Assets/GameFolders/Scripts/ObjectPoolManager.cs
@@ -5,83 +5,102 @@ using UnityEngine;
 
 public enum PoolObjectId
 {
-    MuzzleFlashFx
+    MuzzleFlashFx,
+    Bullet,
+    BulletCasin,
 }
 
-public class ObjectPoolManager : SingletonMonoObject<ObjectPoolManager> 
+namespace Managers
 {
+    public class ObjectPoolManager : SingletonMonoObject<ObjectPoolManager>
+    {
 
-    [Serializable]
-    public struct Pool
-    {
-        public Queue<GameObject> PooledObjects;
-        public GameObject Prefab;
-        public int PoolSize;
-        public PoolObjectId ObjectId;
-    }
-    [SerializeField] private Pool[] _objectPools;
-    private void Awake()
-    {
-        SingletonThisObject(this);
-    }
-
-    private void Start()
-    {
-        InitializeObjectPool();
-    }
-    void InitializeObjectPool()
-    {
-        for (int i = 0; i < _objectPools.Length; i++)
+        [Serializable]
+        public struct Pool
         {
-            _objectPools[i].PooledObjects = new Queue<GameObject>();
+            public Queue<GameObject> PooledObjects;
+            public GameObject Prefab;
+            public int PoolSize;
+            public PoolObjectId ObjectId;
+        }
+        [SerializeField] private Pool[] _objectPools;
+        private void Awake()
+        {
+            SingletonThisObject(this);
+        }
 
-            for (int j = 0; j < _objectPools[i].PoolSize ; j++)
+        private void Start()
+        {
+            InitializeObjectPool();
+        }
+        private void Update()
+        {
+            //foreach (Pool pool in _objectPools)
+            //{
+            //    Debug.Log(pool.ObjectId.ToString() + " " + pool.PooledObjects.Count);
+
+            //}
+        }
+        void InitializeObjectPool()
+        {
+            for (int i = 0; i < _objectPools.Length; i++)
             {
-                GameObject newObj = Instantiate(_objectPools[i].Prefab);
+                _objectPools[i].PooledObjects = new Queue<GameObject>();
+
+                for (int j = 0; j < _objectPools[i].PoolSize; j++)
+                {
+                    GameObject newObj = Instantiate(_objectPools[i].Prefab);
+                    newObj.SetActive(false);
+                    newObj.transform.SetParent(transform);
+                    _objectPools[i].PooledObjects.Enqueue(newObj);
+                }
+            }
+        }
+        public GameObject GetObjectFromPool(Transform newTransform, PoolObjectId poolId)
+        {
+
+            foreach (Pool pool in _objectPools)
+            {
+                if (pool.ObjectId == poolId)
+                {
+                    if (pool.PooledObjects.Count == 0)
+                    {
+                        Debug.Log("Increased " + pool.ObjectId.ToString());
+                        IncreasePoolSize(pool, 3);
+                    }
+                    GameObject gameObj = pool.PooledObjects.Dequeue();
+                    gameObj.transform.position = newTransform.position;
+                    gameObj.transform.rotation = newTransform.rotation;
+                    gameObj.SetActive(true);
+                    return gameObj;
+                }
+            }
+            return null;
+        }
+        public void SetPool(GameObject objToSet, PoolObjectId poolId)
+        {
+            foreach (Pool pool in _objectPools)
+            {
+                if (pool.ObjectId == poolId)
+                {
+
+                    pool.PooledObjects.Enqueue(objToSet);
+                    objToSet.SetActive(false);
+                    objToSet.transform.SetParent(transform);
+
+                }
+            }
+        }
+        private void IncreasePoolSize(Pool pool, int increment)
+        {
+            for (int j = 0; j < increment; j++)
+            {
+                GameObject newObj = Instantiate(pool.Prefab);
                 newObj.SetActive(false);
                 newObj.transform.SetParent(transform);
-                _objectPools[i].PooledObjects.Enqueue(newObj);
+                pool.PooledObjects.Enqueue(newObj);
             }
         }
     }
-    public GameObject GetObjectFromPool(PoolObjectId poolId)
-    {
-        Debug.Log("helo");
-        foreach (Pool pool in _objectPools)
-        {
-            if(pool.ObjectId == poolId)
-            {
-                if(pool.PooledObjects.Count<1)
-                {
-                    IncreasePoolSize(pool,3);
-                }
-                GameObject gameObj = pool.PooledObjects.Dequeue();
-                return gameObj;
-            }
-        }
-        return null; 
-    }
-    public void SetPool(GameObject objToSet, PoolObjectId poolId)
-    {
-        foreach (Pool pool in _objectPools)
-        {
-            if (pool.ObjectId == poolId)
-            {
-                objToSet.SetActive(false);
-                objToSet.transform.SetParent(transform);
-                pool.PooledObjects.Enqueue(objToSet);   
 
-            }
-        }
-    }
-    private void IncreasePoolSize(Pool pool, int increment)
-    {
-        for (int j = 0; j < increment; j++)
-        {
-            GameObject newObj = Instantiate(pool.Prefab);
-            newObj.SetActive(false);
-            newObj.transform.SetParent(transform);
-            pool.PooledObjects.Enqueue(newObj);
-        }
-    }
 }
